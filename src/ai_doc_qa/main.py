ba9e2815dce703499b028d023259f547a94329b0
Darsh-Bothra <darshbothra007@gmail.com>
2026-08-16 20:16:47 +0530

    added basic pdf processing

--- src/ai_doc_qa/main.py
+++ src/ai_doc_qa/main.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from ai_doc_qa.db.db import get_db
 
+from ai_doc_qa.services.document_processing import extract_text
+
 from ai_doc_qa.api.routes.auth import router as auth_router
 from ai_doc_qa.api.routes.documents import router as docs_router
 
@@ -24,23 +26,23 @@ def test_route():
         "message": "Testing route"
     }
 
-# @app.post("/test-upload")
-# async def test_upload(
-#     file: UploadFile
-# ): 
-#     content = await file.read()
-#     file_path = UPLOAD_DIR / file.filename
+@app.post("/test-upload")
+async def test_upload(
+    file: UploadFile
+): 
+    content = await file.read()
+    file_path = UPLOAD_DIR / file.filename
 
-#     with file_path.open("wb") as buffer:
-#         while chunk := await file.read(1024 * 1024):
-#             buffer.write(chunk)
+    with file_path.open("wb") as buffer:
+        while chunk := await file.read(1024 * 1024):
+            buffer.write(chunk)
 
-#     return {
-#         "file_name": file.filename,
-#         "path": str(file_path),
-#         "content_type": file.content_type,
-#         "size": len(content)
-#     }
+    return {
+        "file_name": file.filename,
+        "path": str(file_path),
+        "content_type": file.content_type,
+        "size": len(content)
+    }
 
 
 
@@ -53,3 +55,6 @@ async def test_connection(db: AsyncSession=Depends(get_db)):
         "message": "DB created successfully"
     }
 
+
+text = extract_text("uploaded_documents/test-1.pdf")
+print(text)
